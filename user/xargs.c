4aaf8440a7661eb980ce5111f5d47fadb5f51fd9
yoo2i <yoo2i@qq.com>
2025-03-24 14:15:26 +0800

    achieve lab1

--- user/xargs.c
+++ user/xargs.c
@@ -12,17 +12,16 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {
         args[i - 1] = (char*)malloc(MAXARG);
         strcpy(args[i - 1], argv[i]);
-        //
-        // printf("%s\n", args[i - 1]);
     }
     
     char tmp;
+    /* 如果先让p指向未赋值的args[]则p会为null从而无法运行 *
+     * 注意每一项的实质是字符指针 */
     args[argc - 1] = (char*)malloc(MAXARG);
-    char* p = args[argc - 1]; //
+    char* p = args[argc - 1];
     args[argc] = 0;
     while (read(STDIN_FILENO, &tmp, 1) > 0) {
         if (tmp == '\n') {
-
             if (fork() == 0) {
                 *p = '\0';
                 exec(args[0], args);
@@ -30,16 +29,18 @@ int main(int argc, char* argv[]) {
             } else {
                 wait(0);
                 p = args[argc - 1];
-                //printf("success\n");
             }
         } else {
             *p++ = tmp;
-            //printf("%s\n", args[argc - 1]);
         }
     }
+    
     free(args[argc - 1]);
     for (int i = 1; i < argc; i++) {
         free(args[i - 1]);
     }
+    
     exit(0);
 }
+
+//对字符串操作常用一个指针p指向字符串的末尾
