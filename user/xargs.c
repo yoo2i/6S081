bb4dd70d5698149361d91d6931719118b83fb131
yoo2i <yoo2i@qq.com>
2025-03-24 10:25:08 +0800

    achieve xargs

---
+++ user/xargs.c
@@ -0,0 +1,45 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/param.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(2, "Usage: xargs command\n");
+        exit(1);
+    }
+
+    char* args[argc + 1];
+    for (int i = 1; i < argc; i++) {
+        args[i - 1] = (char*)malloc(MAXARG);
+        strcpy(args[i - 1], argv[i]);
+        //
+        // printf("%s\n", args[i - 1]);
+    }
+    
+    char tmp;
+    args[argc - 1] = (char*)malloc(MAXARG);
+    char* p = args[argc - 1]; //
+    args[argc] = 0;
+    while (read(STDIN_FILENO, &tmp, 1) > 0) {
+        if (tmp == '\n') {
+
+            if (fork() == 0) {
+                *p = '\0';
+                exec(args[0], args);
+                exit(1);
+            } else {
+                wait(0);
+                p = args[argc - 1];
+                //printf("success\n");
+            }
+        } else {
+            *p++ = tmp;
+            //printf("%s\n", args[argc - 1]);
+        }
+    }
+    free(args[argc - 1]);
+    for (int i = 1; i < argc; i++) {
+        free(args[i - 1]);
+    }
+    exit(0);
+}
