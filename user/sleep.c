df54a7e83943efeed7f6212f06bb59f662c51c9c
yoo2i <yoo2i@qq.com>
2025-03-29 20:22:22 +0800

    achieve lab1, clangd and gdb

--- user/sleep.c
+++ user/sleep.c
@@ -1,8 +1,7 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) 
-{
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(2, "Usage: sleep <ticks>\n");
         exit(1);
