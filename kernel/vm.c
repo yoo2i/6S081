651f95fda173097d4a6c541ac65fbdae577ac171
yoo2i <yoo2i@qq.com>
2025-04-07 08:58:50 +0800

    achieve vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void _vmprint(pagetable_t pagetable, int level) {
+	for (int i = 0; i < 512; i++) {
+		pte_t pte = pagetable[i];
+		if (pte & PTE_V) {
+			for (int j = 1; j < level; j++) {
+				printf(".. ");
+			}
+			printf("..");
+
+			uint64 child = PTE2PA(pte);
+			printf("%d: pte %p pa %p\n", i, pte, child);
+			
+			if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) { // pte_v有效且rwx均为零代表为2级或1级页表
+				_vmprint((pagetable_t)child, level + 1);
+			}
+		}
+	}
+	
+}
+void vmprint(pagetable_t pagetable) {
+	printf("page table %p\n", pagetable);
+	_vmprint(pagetable, 1);
+
+}
